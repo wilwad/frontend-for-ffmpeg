4cc571a1a2512abcc1c6afeb6736b5689d96092a
William Sengdara <william.sengdara@gmail.com>
2021-03-23 11:44:55 +0200

    Delete actions.php

--- actions.php
+++
@@ -1,8 +0,0 @@
-$actions = 
-[
-	'deshake'=>"ffmpeg -i input.mov -vf deshake output.mov",
-	'fadeinout-video'=>"ffmpeg -i input.mp4 -vf "fade=t=in:st=0:d=5,fade=t=out:st=10:d=5" -c:a copy output.mp4",
-	'fadeinout-audio'=>"ffmpeg -i input.mp4 -af "afade=t=in:st=0:d=5" -c:v copy output.mp4",
-	'extractaudio'=>"ffmpeg -i input.mp4 -vn -acodec copy output.mp3",
-	'videoremoveaudio'=>"ffmpeg -i videoWithAudio.mp4 -c:v copy -an videoWithoutAudio.mp4",
-];
